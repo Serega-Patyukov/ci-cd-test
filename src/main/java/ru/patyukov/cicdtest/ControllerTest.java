c8e439b5d196343f04f6b03b63c541492b5bb3e3
spatyukov <spatyukov@phoenixit.ru>
2023-09-13 17:13:13 +0300

    fetch

--- src/main/java/ru/patyukov/cicdtest/ControllerTest.java
+++ src/main/java/ru/patyukov/cicdtest/ControllerTest.java
@@ -1,10 +1,15 @@
 package ru.patyukov.cicdtest;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
 
 @Slf4j
 @RestController
@@ -13,6 +18,16 @@ public class ControllerTest {
 
     @GetMapping("/status")
     public @ResponseBody String status() {
+
+        RestTemplate template = new RestTemplate();
+
+        for (int i = 0; i < 2000; i++) {
+            new Thread(() -> template.exchange("http://localhost:27015/" + new Random().nextInt(), HttpMethod.GET, null, String.class)).start();
+//            new Thread(() -> template.exchange("http://localhost:27015/" + new Random().nextInt(), HttpMethod.POST, null, String.class)).start();
+        }
+
+//        ResponseEntity<String> response = template.exchange("http://localhost:8080/", HttpMethod.POST, null, String.class);
+
         return "*** Deploy OK ***";
     }
 
